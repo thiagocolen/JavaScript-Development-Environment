6b0611a590cde36e6699816f4c88da0c37ad852f
thiagocolen <thiago.souzacolen@gmail.com>
2017-07-23 16:01:13 -0300

    add less-loader

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -15,7 +15,7 @@ export default {
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader']},
-      {test: /\.css$/, loaders: ['style-loader', 'css-loader']}
+      {test: /\.less$/, loaders: ['style-loader', 'css-loader', 'less-loader']}
     ]
   }
 }
